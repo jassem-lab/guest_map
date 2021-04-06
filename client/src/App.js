13fdbbad6292df2c35f7d5d2d99e82cc17fdf079
jassem-lab <jassemgaaloul123@gmail.com>
2021-04-06 03:46:08 +0100

    TODO uptodate

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,12 @@ function App() {
   const position = [51.505, -0.09]
   return (
     <div className="App">
-      <MapContainer center={[51.505, -0.09]} className="map" zoom={13} scrollWheelZoom={false}>
+      <MapContainer center={position} className="map" zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
+        <Marker position={position}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
     </Popup>
