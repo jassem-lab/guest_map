b10096ec670f6669791dd85f3c21f5155b477f76
jassem-lab <jassemgaaloul123@gmail.com>
2021-04-08 04:24:48 +0100

    still fixing joi validation issue

--- backend/src/api/messages.js
+++ backend/src/api/messages.js
@@ -1,23 +1,46 @@
+
 const express = require('express');
 const Joi = require('joi');
-const router = express.Router();
+
+const db = require('../db');
+const messages = db.get('messages');
 
 const schema = Joi.object().keys({
-  name: Joi.string().alphanum().min(3).max(30).required(),
-  message: Joi.string().alphanum().min(3).max(5000).required(),
+  name: Joi.string().min(1).max(500).required(),
+  message: Joi.string().min(1).max(500).required(),
   latitude: Joi.number().min(-90).max(90).required(),
-  longitude: Joi.number().min(-180).max(180).required(),
-  date: Joi.date(),
-
-})
-
-router.get('/', (req, res) => {
-  res.json(['😀', '😳', '🙄']);
+  longitude: Joi.number().min(-180).max(180).required()
 });
 
+const router = express.Router();
+
 router.get('/', (req, res) => {
-  
-  res.json(['😀', '😳', '🙄']);
+  messages
+    .find()
+    .then(allMessages => {
+      res.json(allMessages);
+    });
 });
 
+router.post('/',  (req, res, next) => {
+  const result =  schema.validateAsync(req.body)
+  if (result.error === null) {
+    const { name, message, latitude, longitude } = req.body;
+    const userMessage = {
+      name,
+      message,
+      latitude,
+      longitude,
+      date: new Date()
+    };
+    next(schema.validate(req.body))
+    messages
+      .insert(userMessage)
+      .then(insertedMessage => {
+        res.json(insertedMessage);
+      });
+  } else {
+    next(result.error);
+  }
+});
 module.exports = router;
--- backend/test/api.test.js
+++ backend/test/api.test.js
@@ -9,17 +9,61 @@ describe('GET /api/v1', () => {
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200, {
-        message: 'API - 👋🌎🌍🌏'
+        message: 'API - 👋🌎🌍🌏' 
       }, done);
   });
 });
 
-describe('GET /api/v1/emojis', () => {
-  it('responds with a json message', (done) => {
+describe('POST /api/v1/messages', () => {
+  it('responds with inserted message', (done) => {
+    const requestObj = {
+      name: 'jassem',
+      message: 'This app is so cool!',
+      latitude: -90,
+      longitude: 180
+    };
+
+    const responseObj = {
+      ...requestObj,
+      _id: '5b57d127923211248855977c',
+      date: '2018-07-25T01:23:51.029Z'
+    };
+
     request(app)
-      .get('/api/v1/emojis')
+      .post('/api/v1/messages')
+      .send(requestObj)
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
-      .expect(200, ['😀', '😳', '🙄'], done);
+      .expect(res => {
+        res.body._id = '5b57d127923211248855977c';
+        res.body.date = '2018-07-25T01:23:51.029Z';
+      })
+      .expect(200, responseObj, done);
+  });
+
+  it('can signup with a name that has diacritics', (done) => {
+    const requestObj = {
+      name: 'jassem',
+      message: 'This app is so cool!',
+      latitude: -90,
+      longitude: 180
+    };
+
+    const responseObj = {
+      ...requestObj,
+      _id: '5b57d127923211248855977c',
+      date: '2018-07-25T01:23:51.029Z'
+    };
+
+    request(app)
+      .post('/api/v1/messages')
+      .send(requestObj)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(res => {    
+        res.body._id = '5b57d127923211248855977c';
+        res.body.date = '2018-07-25T01:23:51.029Z';
+      })
+      .expect(200, responseObj, done);
   });
 });
